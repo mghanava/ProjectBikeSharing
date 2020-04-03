bfb68cfad3fe85e8047f372e2609bc9946b8ce87
mghanava <mghanava@hotmail.com>
2020-04-02 19:54:36 -0600

    clean file

--- my_answers.py
+++ my_answers.py
@@ -68,8 +68,7 @@ class NeuralNetwork(object):
 
         # TODO: Output layer - Replace these values with your calculations.
         final_inputs = np.matmul(hidden_outputs, self.weights_hidden_to_output) # signals into final output layer
-        # Despite hidden layer, sigmoid activation function is replaced with activation function of f(x)=x as we are not trying to get a probablity but a real
-        # number (count of rides). So in this case the final output will be equal to final input.
+        # Despite hidden layer, sigmoid activation function is replaced with activation function of f(x)=x as we are not trying to get a probablity but a real number (count of rides). So in this case the final output will be equal to final input.
         final_outputs = final_inputs # signals from final output layer
         
         return final_outputs, hidden_outputs
@@ -100,19 +99,18 @@ class NeuralNetwork(object):
         
         # each column element represents error contibution of each hidden node for each record  
         hidden_error = np.matmul(output_error_term, self.weights_hidden_to_output.T)
-#         hidden_error = error * self.weights_hidden_to_output
+        # hidden_error = error * self.weights_hidden_to_output
         # derivative of hidden layer activation function ddxσ(x)=σ(x)(1−σ(x)) 
         hidden_error_term = hidden_error * (hidden_outputs * (1.0 - hidden_outputs))
-#         hidden_error_term = np.transpose(hidden_error) * (hidden_outputs * (1.0 - hidden_outputs))
+        # hidden_error_term = np.transpose(hidden_error) * (hidden_outputs * (1.0 - hidden_outputs))
         
         # Weight step (input to hidden)
         delta_weights_i_h += np.matmul(X[:, None], hidden_error_term)
-#         delta_weights_i_h += np.matmul(X[:, None], hidden_error_term)
-#         delta_weights_i_h += hidden_error_term * X[:, None]
+        # delta_weights_i_h += hidden_error_term * X[:, None]
         # Weight step (hidden to output)
         delta_weights_h_o += np.matmul(hidden_outputs.T, output_error_term)
-#         delta_weights_h_o += output_error_term*np.transpose(hidden_outputs)
-#         delta_weights_h_o += output_error_term * hidden_outputs[:, None]
+        # delta_weights_h_o += output_error_term*np.transpose(hidden_outputs)
+
         return delta_weights_i_h, delta_weights_h_o
 
     def update_weights(self, delta_weights_i_h, delta_weights_h_o, n_records):
